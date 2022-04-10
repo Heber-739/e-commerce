6eff887ab100e18e6fe47d9aa448e48b8843b792
Heber-739 <Heberduarteryr@gmail.com>
2022-04-10 14:43:09 -0300

    Home desktop Terminado

---
+++ javascript/descripciones.js
@@ -0,0 +1,29 @@
+export class StarWars {
+  nombre;
+  url;
+  descripcion;
+  precio;
+  identificador;
+}
+
+export class Consola {
+  nombre;
+  url;
+  descripcion;
+  precio;
+  identificador;
+}
+export class Accesorio {
+  nombre;
+  url;
+  descripcion;
+  precio;
+  identificador;
+}
+export class Otros {
+  nombre;
+  url;
+  descripcion;
+  precio;
+  identificador;
+}
